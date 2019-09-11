1e01340ebf9319623e9a653bb04ad38271a4c103
lss <1332456550@qq.com>
2019-09-11 15:04:26 +0800

    修改图片域名cmd

--- new_shop/config.js
+++ new_shop/config.js
@@ -1,18 +1,18 @@
 export default {
   // 正式域名
-  // base_url: `https://api.olb8.com`,
+  base_url: `https://api.olb8.com`,
   //测试
   // base_url:'https://api.test.olb8.com',
   // 本地
   // base_url: `http://192.168.1.191:9082`,
   // base_url: `http://192.168.3.181:9081`,
   // 广东本地
-  base_url: `http://134.175.171.231:9081`,
+  // base_url: `http://134.175.171.231:9081`,
  // 图片前缀正式
   // IMG: 'https://api.test.olb8.com',
   // 图片前缀正式
-  // IMG : "https://api.olb8.com"
+  IMG: "http://cdn.img.olb8.com"
   // 图片前缀正式
   // IMG: `http://192.168.1.191:9081`
-  IMG: 'http://134.175.171.231:9081'
+  // IMG: 'http://134.175.171.231:9081'
 }
